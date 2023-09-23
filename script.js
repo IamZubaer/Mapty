97139d1658a1623078e8025a6c9f3268e4b8e547
Talha <zubaer.talha20@gmail.comm>
2023-09-23 23:59:14 +0600

    Implemented geolocation and map

---
+++ script.js
@@ -0,0 +1,39 @@
+'use strict';
+
+// prettier-ignore
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form__input--type');
+const inputDistance = document.querySelector('.form__input--distance');
+const inputDuration = document.querySelector('.form__input--duration');
+const inputCadence = document.querySelector('.form__input--cadence');
+const inputElevation = document.querySelector('.form__input--elevation');
+
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      const coords = [latitude, longitude];
+      console.log(position);
+      console.log(longitude, latitude);
+      var map = L.map('map').setView(coords, 13);
+
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      map.on('click', function (e) {
+        const { lat, lng } = e.latlng;
+        console.log(lat, lng);
+        L.marker([lat, lng]).addTo(map).bindPopup('Click').openPopup();
+      });
+    },
+
+    function () {
+      alert(`Could not get you position!`);
+    }
+  );
