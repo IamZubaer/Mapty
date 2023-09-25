5e55138750d11e98f46871d128c06eb9e3585898
Talha <zubaer.talha20@gmail.comm>
2023-09-25 17:41:23 +0600

    Finally done

--- script.js
+++ script.js
@@ -161,7 +161,7 @@ class App {
       .openPopup();
   }
   _renderWorkout(workout) {
-    let html = `
+    let html = ` 
     <li class="workout workout--${workout.type}" data-id="${workout.id}">
           <h2 class="workout__title">${workout.description}</h2>
           <div class="workout__details">
